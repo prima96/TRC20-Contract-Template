d457b1bd59e8953018fa3d19d6a28ee5611ed76f
prima96 <85532031+prima96@users.noreply.github.com>
2021-06-11 11:15:14 +0700

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("BOGE COIN", "BGC", 2) {
-        _mint(msg.sender, 10000000 * (2 ** uint256(decimals())));
+    constructor () public ERC20Detailed("ORION", "ORION", 18) {
+        _mint(msg.sender, 100000000 * (10 ** uint256(decimals())));
     }
 }
