bfe7831610c465f4d9135bd192a9fff7be36dda0
prima96 <85532031+prima96@users.noreply.github.com>
2021-06-18 13:44:46 +0700

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -2,21 +2,21 @@
 // Enable optimization
 pragma solidity ^0.5.0;
 
-import "./TRC20.sol";
-import "./TRC20Detailed.sol";
+import "./ERC20.sol";
+import "./ERC20Detailed.sol";
 
 /**
  * @title SimpleToken
- * @dev Very simple TRC20 Token example, where all tokens are pre-assigned to the creator.
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `ERC20` functions.
  */
-contract Token is TRC20, TRC20Detailed {
+contract Token is ERC20, ERC20Detailed {
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("PLUTOS", "PTS", 18) {
-        _mint(msg.sender, 100000000 * (10 ** uint256(decimals())));
+    constructor () public ERC20Detailed("PLUTOS", "PTS", 10) {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
     }
 }
