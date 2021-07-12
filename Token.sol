7c5ce5979edfec2e38c296034cebaca6894b9660
prima96 <85532031+prima96@users.noreply.github.com>
2021-07-12 09:56:50 +0700

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("GEMBITES", "GBTS", 10) {
-        _mint(msg.sender, 100000 * (10 ** uint256(decimals())));
+    constructor () public ERC20Detailed("EZOCH", "EZOCH", 10) {
+        _mint(msg.sender, 50000 * (10 ** uint256(decimals())));
     }
 }
