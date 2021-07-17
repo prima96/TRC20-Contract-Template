7bf0538d5784ee526d989429c6778f7ca0ae31e1
prima96 <85532031+prima96@users.noreply.github.com>
2021-07-17 23:18:37 +0700

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("SmartWayGlobal", "SWG", 10) {
-        _mint(msg.sender, 25000 * (10 ** uint256(decimals())));
+    constructor () public ERC20Detailed("RemiChain", "RMC", 10) {
+        _mint(msg.sender, 10000 * (10 ** uint256(decimals())));
     }
 }
